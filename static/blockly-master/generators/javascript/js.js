452e84aae3a44cc183de5a5f83e5bfa1ad670ee9
MathCalculus <code_math_calculus@163.com>
2022-02-05 12:19:07 +0000

    update static/blockly-master/generators/javascript/js.js.

--- static/blockly-master/generators/javascript/js.js
+++ static/blockly-master/generators/javascript/js.js
@@ -4,16 +4,16 @@ Blockly.JavaScript["js_usestrict"] = function(block){
 }
 
 Blockly.JavaScript["js_bitwise"] = function(block){
-    var value1 = block.getFieldValue('VALUE1');
-    var value2 = block.getFieldValue('VALUE2');
+    var value1 = Blockly.JavaScript.valueToCode(block, 'VALUE1', Blockly.JavaScript.ORDER_ADDITION) || "0";
+    var value2 = Blockly.JavaScript.valueToCode(block, 'VALUE2', Blockly.JavaScript.ORDER_ADDITION) || "0";
     var operation_type = block.getFieldValue('TYPE');
     let code = value1 + " " + operation_type + " " + value2;
     return code;
 }
 
 Blockly.JavaScript["js_comparisons"] = function(block){
-    var value1 = block.getFieldValue('VALUE1');
-    var value2 = block.getFieldValue('VALUE2');
+    var value1 = Blockly.JavaScript.valueToCode(block, 'VALUE1', Blockly.JavaScript.ORDER_ADDITION) || "0";
+    var value2 = Blockly.JavaScript.valueToCode(block, 'VALUE2', Blockly.JavaScript.ORDER_ADDITION) || "0";
     var comparisons_type = block.getFieldValue('TYPE');
     let code = value1 + " " + comparisons_type + " " + value2
     return code;
@@ -25,19 +25,19 @@ Blockly.JavaScript["js_time"] = function(block){
 }
 
 Blockly.JavaScript["js_popup_alert"] = function(block){
-    var text = block.getFieldValue('TEXT');
-    var code = 'alert("'+text+'");';
+    var text = Blockly.JavaScript.valueToCode(block, 'TEXT', Blockly.JavaScript.ORDER_ADDITION) || "null";
+    var code = 'alert('+text+');';
     return code;
 }
 
 Blockly.JavaScript["js_popup_confirm"] = function(block){
-    var text = block.getFieldValue('TEXT');
-    var code = 'confirm("'+text+'")';
+    var text = Blockly.JavaScript.valueToCode(block, 'TEXT', Blockly.JavaScript.ORDER_ADDITION) || "null";
+    var code = 'confirm('+text+')';
     return code;
 }
 
 Blockly.JavaScript["js_popup_prompt"] = function(block){
-    var text = block.getFieldValue('TEXT');
-    var code = 'prompt("'+text+'")';
+    var text = Blockly.JavaScript.valueToCode(block, 'TEXT', Blockly.JavaScript.ORDER_ADDITION) || "null";
+    var code = 'prompt('+text+')';
     return code;
 }
