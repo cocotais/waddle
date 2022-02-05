4fa7cc0ab66a405323c60d550df2ef182d10699a
中子星000 <f12y34m56@163.com>
2022-02-05 13:49:00 +0800

    改链接

--- static/blockly-master/generators/javascript/control.js
+++ static/blockly-master/generators/javascript/control.js
@@ -1,8 +1,10 @@
-/* goog.module('Blockly.JavaScript.control');
-goog.require('Blockly.JavaScript');
-goog.require('Blockly');
+
+'use strict';
+
+goog.module('Blockly.JavaScript.control');
+
 const JavaScript = goog.require('Blockly.JavaScript');
- */
+
 JavaScript["control_invisible_craft"] = function(block) {
     let name = block.getFieldValue('NAME');
     let content = block.getFieldValue('CONTENT');
