6940291a38303c2784631c69edc2d593f09834e3
中子星000 <f12y34m56@163.com>
2022-02-05 17:55:57 +0800

    第一次实现！！！

---
+++ block_xmls/history/invisibleWidget (1).js
@@ -0,0 +1,28 @@
+Blockly.JavaScript['invisiblewidgettypes'] = function(block) {
+  var text_type = block.getFieldValue('type');
+  var text_icon = block.getFieldValue('icon');
+  var text_title = block.getFieldValue('title');
+  var checkbox_isglobalwidget = block.getFieldValue('isGlobalWidget') === 'TRUE';
+  var value_properties = Blockly.JavaScript.valueToCode(block, 'properties', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_methods = Blockly.JavaScript.valueToCode(block, 'methods', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_event = Blockly.JavaScript.valueToCode(block, 'event', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['invisiblewidgetclass'] = function(block) {
+  var statements_constructor = Blockly.JavaScript.statementToCode(block, 'constructor');
+  var value_functions = Blockly.JavaScript.valueToCode(block, 'functions', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['invisiblewidget'] = function(block) {
+  var statements_invisiblewidgettypes = Blockly.JavaScript.statementToCode(block, 'invisibleWidgetTypes');
+  var statements_invisiblewidgetclass = Blockly.JavaScript.statementToCode(block, 'invisibleWidgetClass');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
--- static/blockly-master/generators/javascript/invisibleWidget.js
+++ static/blockly-master/generators/javascript/invisibleWidget.js
@@ -1,28 +1,59 @@
-Blockly.JavaScript['ivw_invisiblewidgettypes'] = function(block) {
-  var text_type = block.getFieldValue('type');
-  var text_icon = block.getFieldValue('icon');
-  var text_title = block.getFieldValue('title');
-  var checkbox_isglobalwidget = block.getFieldValue('isGlobalWidget') === 'TRUE';
-  var value_properties = Blockly.JavaScript.valueToCode(block, 'properties', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_methods = Blockly.JavaScript.valueToCode(block, 'methods', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_event = Blockly.JavaScript.valueToCode(block, 'event', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
+Blockly.JavaScript['ivw_invisiblewidget'] = function (block) {
+    var text_type = block.getFieldValue('type');
+    var text_icon = block.getFieldValue('icon');
+    var text_title = block.getFieldValue('title');
+    var checkbox_isglobalwidget = block.getFieldValue('isGlobalWidget') === 'TRUE';
+    var value_properties = Blockly.JavaScript.valueToCode(block, 'properties', Blockly.JavaScript.ORDER_ATOMIC);
+    var value_methods = Blockly.JavaScript.valueToCode(block, 'methods', Blockly.JavaScript.ORDER_ATOMIC);
+    var value_event = Blockly.JavaScript.valueToCode(block, 'event', Blockly.JavaScript.ORDER_ATOMIC);
+    var statements_constructor = Blockly.JavaScript.statementToCode(block, 'constructor');
+    var value_functionslist = Blockly.JavaScript.valueToCode(block, 'functionsList', Blockly.JavaScript.ORDER_ATOMIC);
+    // TODO: Assemble JavaScript into code variable.
+    var code = 
+`
+/*
+* 嘿，欢迎使用CoCoMake制作控件
+* 本工具由MathCalculus（QQ：2504556268）、中子星000（QQ：2422481178）制作
+*/
+
+const TYPE = '${text_type}';
+const WIDGET_ICON = '${text_icon}';
+const TITLE = '${text_title}';
+
+const types = {
+	type: TYPE,
+	icon: WIDGET_ICON,
+	title: TITLE,
+	// version: '1.0.0',
+	// platforms: ['android', 'ios', 'web'],
+	isInvisibleWidget: true,
+	isGlobalWidget: ${checkbox_isglobalwidget},
+	docs: {
+		url: 'https://coco.codemao.cn'
+	},
+	properties: [],
+	methods: [],
+	events: []
 };
 
-Blockly.JavaScript['ivw_invisiblewidgetclass'] = function(block) {
-  var statements_constructor = Blockly.JavaScript.statementToCode(block, 'constructor');
-  var value_functions = Blockly.JavaScript.valueToCode(block, 'functions', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+class Widget extends InvisibleWidget {
+	constructor(props) {
+		super(props);
+	}
+}
 
-Blockly.JavaScript['ivw_invisiblewidget'] = function(block) {
-  var statements_invisiblewidgettypes = Blockly.JavaScript.statementToCode(block, 'invisibleWidgetTypes');
-  var statements_invisiblewidgetclass = Blockly.JavaScript.statementToCode(block, 'invisibleWidgetClass');
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
+console.log('* 嘿，欢迎使用CoCoMake制作控件');
+console.log('* 本工具由MathCalculus（QQ：2504556268）、中子星000（QQ：2422481178）制作');
+
+exports.types = types;
+exports.widget = Widget;
+
+
+
+
+
+
+`
+    ;
+    return code;
 };
