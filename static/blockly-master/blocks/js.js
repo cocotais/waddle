c49bce035cfdbe2aa21720b9db65061f06fc4d81
MathCalculus <code_math_calculus@163.com>
2022-02-08 00:45:30 +0000

    update static/blockly-master/blocks/js.js.

--- static/blockly-master/blocks/js.js
+++ static/blockly-master/blocks/js.js
@@ -340,3 +340,18 @@ Blockly.Blocks['js_console_timeend'] = {
         this.setHelpUrl("https://www.runoob.com/w3cnote/javascript-console-object.html");
     }
 };
+
+Blockly.Blocks['js_base64'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("Base64转换")
+            .appendField(new Blockly.FieldDropdown([["ASCII字符", "ASCII"], ["非ASCII字符", "UNASCII"]]), "TYPE_CODE")
+            .appendField(new Blockly.FieldDropdown([["编码", "BM"], ["解码", "JM"]]), "TYPE");
+        this.appendValueInput("TEXT")
+            .setCheck("String");
+        this.setOutput(true, "String");
+        this.setColour(230);
+        this.setTooltip("进行base64转换");
+        this.setHelpUrl("");
+    }
+};
