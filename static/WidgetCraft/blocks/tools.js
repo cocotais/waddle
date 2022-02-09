75a2cbc0e90d154718cbdff625c457d3eba5278b
中子星000 <f12y34m56@163.com>
2022-02-09 17:23:44 +0800

    tools积木盒改颜色

--- static/WidgetCraft/blocks/tools.js
+++ static/WidgetCraft/blocks/tools.js
@@ -9,6 +9,7 @@ Blockly.Blocks['tools_blocks_clip'] = {
         this.setNextStatement(true, null);
         this.setTooltip("工具.积木夹子：方便积木分段，折叠；顶部用于插入注释");
         this.setHelpUrl("");
+        this.setColour("#9429FF");
     }
 };
 
@@ -20,6 +21,7 @@ Blockly.Blocks['tools_only_run'] = {
         this.setNextStatement(true, null);
         this.setTooltip("工具.仅运行：用于运行有返回值的积木");
         this.setHelpUrl("");
+        this.setColour("#9429FF");
     }
 };
 
@@ -30,5 +32,6 @@ Blockly.Blocks['tools_exegesis'] = {
         this.setOutput(true, null);
         this.setTooltip("工具.注释：用于插入单行注释");
         this.setHelpUrl("");
+        this.setColour("#9429FF");
     }
 };
