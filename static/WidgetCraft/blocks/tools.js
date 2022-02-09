7d210fb94b9db96574295bd223765e3ffce1ad95
中子星000 <f12y34m56@163.com>
2022-02-09 17:16:33 +0800

    增加积木盒 工具

---
+++ static/WidgetCraft/blocks/tools.js
@@ -0,0 +1,34 @@
+
+Blockly.Blocks['tools_blocks_clip'] = {
+    init: function () {
+        this.appendValueInput("exegesis")
+            .setCheck(null);
+        this.appendStatementInput("codes")
+            .setCheck(null);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip("工具.积木夹子：方便积木分段，折叠；顶部用于插入注释");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['tools_only_run'] = {
+    init: function () {
+        this.appendValueInput("codes")
+            .setCheck(null);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip("工具.仅运行：用于运行有返回值的积木");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['tools_exegesis'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldTextInput("注释"), "exegesis");
+        this.setOutput(true, null);
+        this.setTooltip("工具.注释：用于插入单行注释");
+        this.setHelpUrl("");
+    }
+};
--- static/WidgetCraft/generators/javascript/tools.js
+++ static/WidgetCraft/generators/javascript/tools.js
@@ -1,6 +1,20 @@
 
-Blockly.JavaScript['tools_blocksClip'] = function (block) {
+Blockly.JavaScript['tools_blocks_clip'] = function (block) {
+    var value_exegesis = Blockly.JavaScript.valueToCode(block, 'exegesis', Blockly.JavaScript.ORDER_ATOMIC);
     var statements_codes = Blockly.JavaScript.statementToCode(block, 'codes');
-    var code = statements_codes;
-    return [code, Blockly.JavaScript.ORDER_NONE];
+    var code = value_exegesis + statements_codes;
+    return code + '\n';
 };
+
+
+Blockly.JavaScript['tools_only_run'] = function (block) {
+    var value_codes = Blockly.JavaScript.valueToCode(block, 'codes', Blockly.JavaScript.ORDER_ATOMIC);
+    var code = value_codes;
+    return code + ';\n';
+};
+
+Blockly.JavaScript['tools_exegesis'] = function(block) {
+    var text_exegesis = block.getFieldValue('exegesis');
+    var code = '// ' + text_exegesis + '\n'
+    return [code, 0];
+  };
