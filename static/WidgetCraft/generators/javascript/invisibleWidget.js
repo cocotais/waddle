ca8fa1629c1dfa3fe497982885c413647e1a6be4
中子星000 <f12y34m56@163.com>
2022-02-11 11:44:25 +0800

    更新了一些属性

--- static/WidgetCraft/generators/javascript/invisibleWidget.js
+++ static/WidgetCraft/generators/javascript/invisibleWidget.js
@@ -16,3 +16,27 @@ Blockly.JavaScript['ivw_itemType'] = function (block) {
     var code = `type: "${text_type}",\n`;
     return code;
 };
+
+Blockly.JavaScript['ivw_itemIcon'] = function (block) {
+    var text_icon = block.getFieldValue('icon');
+    var code = `icon: "${text_icon}",\n`;
+    return code;
+};
+
+Blockly.JavaScript['ivw_itemTitle'] = function (block) {
+    var text_title = block.getFieldValue('title');
+    var code = `title: "${text_title}",\n`;
+    return code;
+};
+
+Blockly.JavaScript['ivw_itemVersion'] = function (block) {
+    var text_title = block.getFieldValue('version');
+    var code = `version: "${text_title}",\n`;
+    return code;
+};
+
+Blockly.JavaScript['ivw_itemIsGlobalWidget'] = function (block) {
+    var checkbox_name = block.getFieldValue('isGlobalWidget') === 'TRUE';
+    var code = `isGlobalWidget: ${checkbox_name},\n`;
+    return code;
+};
