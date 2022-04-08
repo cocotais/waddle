06d0034408d04157b942a2dd714d9d0f8f2638ab
中子星000 <f12y34m56@163.com>
2022-04-08 08:30:05 +0800

    控件配置并入一块积木完成

--- static/Waddle/blocks/invisibleWidget.js
+++ static/Waddle/blocks/invisibleWidget.js
@@ -5,8 +5,22 @@ Blockly.Blocks['ivw_defTypes'] = {
 	init: function () {
 		this.appendDummyInput()
 			.appendField("创建不可见控件");
-		this.appendStatementInput("types")
-			.setCheck(null);
+		this.appendDummyInput()
+			.appendField("类型名")
+			.appendField(new Blockly.FieldTextInput("MY_WIDGET"), "type")
+			.appendField("图标")
+			.appendField(new Blockly.FieldTextInput("https://waddle.coco-central.cn/static/img/logo.svg"), "icon");
+		// 五个参数分两行输入
+		this.appendDummyInput()
+			.appendField("名称")
+			.appendField(new Blockly.FieldTextInput("我的控件"), "type")
+			.appendField("版本")
+			.appendField(new Blockly.FieldTextInput("1.0.0"), "type")
+			.appendField("类型")
+			.appendField(new Blockly.FieldDropdown([["全局控件", "true"], ["屏幕控件", "false"]]), "isGlobalWidget");
+		// this.appendStatementInput("types")
+		// 	.setCheck(null); //弃用
+		this.setPreviousStatement(true, null);
 		this.setNextStatement(true, null);
 		this.setColour("#8647F3");
 		this.setTooltip("定义不可见控件类型，在添加积木中添加积木");
@@ -14,70 +28,70 @@ Blockly.Blocks['ivw_defTypes'] = {
 	}
 };
 
-Blockly.Blocks['ivw_itemType'] = {
-	init: function () {
-		this.appendDummyInput()
-			.appendField("控件类型名")
-			.appendField(new Blockly.FieldTextInput("MY_WIDGET"), "type");
-		this.setPreviousStatement(true, null);
-		this.setNextStatement(true, null);
-		this.setColour("#5574F8");
-		this.setTooltip("type属性，控件类型，必须唯一");
-		this.setHelpUrl(HELP_URL);
-	}
-};
+// Blockly.Blocks['ivw_itemType'] = {
+// 	init: function () {
+// 		this.appendDummyInput()
+// 			.appendField("控件类型名")
+// 			.appendField(new Blockly.FieldTextInput("MY_WIDGET"), "type");
+// 		this.setPreviousStatement(true, null);
+// 		this.setNextStatement(true, null);
+// 		this.setColour("#5574F8");
+// 		this.setTooltip("type属性，控件类型，必须唯一");
+// 		this.setHelpUrl(HELP_URL);
+// 	}
+// };
 
-Blockly.Blocks['ivw_itemIcon'] = {
-	init: function () {
-		this.appendDummyInput()
-			.appendField("控件图标")
-			.appendField(new Blockly.FieldTextInput("https://waddle.coco-central.cn/static/img/logo.svg"), "icon");
-		this.setPreviousStatement(true, null);
-		this.setNextStatement(true, null);
-		this.setColour("#5574F8");
-		this.setTooltip("icon属性，控件图标，url");
-		this.setHelpUrl(HELP_URL);
-	}
-};
+// Blockly.Blocks['ivw_itemIcon'] = {
+// 	init: function () {
+// 		this.appendDummyInput()
+// 			.appendField("控件图标")
+// 			.appendField(new Blockly.FieldTextInput("https://waddle.coco-central.cn/static/img/logo.svg"), "icon");
+// 		this.setPreviousStatement(true, null);
+// 		this.setNextStatement(true, null);
+// 		this.setColour("#5574F8");
+// 		this.setTooltip("icon属性，控件图标，url");
+// 		this.setHelpUrl(HELP_URL);
+// 	}
+// };
 
-Blockly.Blocks['ivw_itemTitle'] = {
-	init: function () {
-		this.appendDummyInput()
-			.appendField("控件名称")
-			.appendField(new Blockly.FieldTextInput("我的控件"), "title");
-		this.setPreviousStatement(true, null);
-		this.setNextStatement(true, null);
-		this.setColour("#5574F8");
-		this.setTooltip("title属性，显示出来的名称");
-		this.setHelpUrl(HELP_URL);
-	}
-};
+// Blockly.Blocks['ivw_itemTitle'] = {
+// 	init: function () {
+// 		this.appendDummyInput()
+// 			.appendField("控件名称")
+// 			.appendField(new Blockly.FieldTextInput("我的控件"), "title");
+// 		this.setPreviousStatement(true, null);
+// 		this.setNextStatement(true, null);
+// 		this.setColour("#5574F8");
+// 		this.setTooltip("title属性，显示出来的名称");
+// 		this.setHelpUrl(HELP_URL);
+// 	}
+// };
 
-Blockly.Blocks['ivw_itemVersion'] = {
-	init: function () {
-		this.appendDummyInput()
-			.appendField("控件版本")
-			.appendField(new Blockly.FieldTextInput("1.0.0"), "version");
-		this.setPreviousStatement(true, null);
-		this.setNextStatement(true, null);
-		this.setColour("#5574F8");
-		this.setTooltip("version属性，版本号x.y.z");
-		this.setHelpUrl(HELP_URL);
-	}
-};
+// Blockly.Blocks['ivw_itemVersion'] = {
+// 	init: function () {
+// 		this.appendDummyInput()
+// 			.appendField("控件版本")
+// 			.appendField(new Blockly.FieldTextInput("1.0.0"), "version");
+// 		this.setPreviousStatement(true, null);
+// 		this.setNextStatement(true, null);
+// 		this.setColour("#5574F8");
+// 		this.setTooltip("version属性，版本号x.y.z");
+// 		this.setHelpUrl(HELP_URL);
+// 	}
+// };
 
-Blockly.Blocks['ivw_itemIsGlobalWidget'] = {
-	init: function () {
-		this.appendDummyInput()
-			.appendField("控件类型")
-			.appendField(new Blockly.FieldDropdown([["全局控件", "true"], ["屏幕控件", "false"]]), "isGlobalWidget");
-		this.setPreviousStatement(true, null);
-		this.setNextStatement(true, null);
-		this.setColour("#5574F8");
-		this.setTooltip("isGlobalWidget属性，是否为全局控件");
-		this.setHelpUrl(HELP_URL);
-	}
-};
+// Blockly.Blocks['ivw_itemIsGlobalWidget'] = {
+// 	init: function () {
+// 		this.appendDummyInput()
+// 			.appendField("控件类型")
+// 			.appendField(new Blockly.FieldDropdown([["全局控件", "true"], ["屏幕控件", "false"]]), "isGlobalWidget");
+// 		this.setPreviousStatement(true, null);
+// 		this.setNextStatement(true, null);
+// 		this.setColour("#5574F8");
+// 		this.setTooltip("isGlobalWidget属性，是否为全局控件");
+// 		this.setHelpUrl(HELP_URL);
+// 	}
+// };
 
 Blockly.Blocks['ivw_addProperty'] = {
 	init: function () {
