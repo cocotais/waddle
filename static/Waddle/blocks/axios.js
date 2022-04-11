62489237bd2da7632e94445ff93a8d6457534e7f
MathCalculus <code_math_calculus@163.com>
2022-04-11 09:30:41 +0800

    防bug

--- static/Waddle/blocks/axios.js
+++ static/Waddle/blocks/axios.js
@@ -1,3 +1,31 @@
+//ooooooooo@^ ... @OooO\.]]/@oo@@@@@oO@ ... @@^****
+//oooooooOOO^ ... @OooO@@@OoooooOOOooO^ ...=@@^****
+//ooooooOOoO@  .. @@Oooooo@OoO@OoooOOO^ ...@O@\****
+//OOOOOOOOOO@` .. @OOOOoooOoooO@@@Ooo@^.../@OO@***,
+//OOOOOOOOOOO\ .. @OoooooooooooooO@OO@^..=@OOO@*,O[
+//OOOOOOOOOOO@`.. @OOO@@@@@@@@@@@@@@O@^..@OOOO@**,]
+//@@@@@OOOOOOO\ ,//`               .[\@`/@OOO@@^,[*
+//@@@@@@@@@OOO@/.                      .\@O@Oo@^O@o
+//@@@@@@OOOO@/ =@@`                    .. =@OO@^***
+//@@OOOOOOO@`  ,[[      =^            =@@^ .@O@\*,]
+//@O@OOOOO@` ......    ,/  =\  .@           .@O\[,*
+//@O@OOOO@^          [[.[O/[,@@@`    ....... =@,[[*
+//@O@O\OO@^                                  =@O@@o
+//@@@@@@@@`                                  =@@@@o
+//O@OO,\`O^                                  @O*`/\
+//\O@/[`/@@^        /@]]                    /O=O/OO
+//****`=OO@@@`..../@o/ooO@.           ....,@/,,[[[*
+//],`****,`\`\@@@@O/o\ooO@............,]@@/]`]]]]]]
+//OO@O\*^*`**]O\./@oO@Oo@@]]]]/O@@@@@@OOOOOOOOOOOOO
+//OOO\/*,]]ooOO@OOOO@@@@/........    =@[[[[[`******
+//,]oOOOO@@@@O/\@@OOO@@`           @[\@O@\******`**
+//@@/[[`******=@Oo@O@@^            @@O@@@@O@@[O//\@
+//*****]=O,O^*O@O@@@O@            .@@@@@OO@@@@\@O`/
+//*****\o^[***,O@@@/[@@@@@@@@@@@@@@[[@@@O@@@/,`,*[`
+//**************]/OOO@^  =@OO@` ./@OOOOO@/*********
+//                 于勤保佑 无bug
+
+
 Blockly.Blocks['axios_import'] = {
     init: function () {
         this.appendDummyInput()
