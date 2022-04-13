94a4798337e0ca12648be3a69e3df3d2b3bc29e2
Cpointerz <3489840593@qq.com>
2022-04-13 16:16:09 +0800

    添加管理工具

---
+++ waddleup/src/main.rs
@@ -0,0 +1,70 @@
+use std::process::Command;
+use anyhow::Result;
+use clap::{App, Arg};
+
+fn main() -> Result<()> {
+    let waddleup = App::new("waddleup")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Cpointerz")
+        .about("waddleup是waddle的官方管理工具")
+        .subcommand(
+            //INSTALL
+            App::new("install")
+            .about("安装waddleup")
+            .arg(
+                Arg::new("stable")
+                .required(true)
+                .index(1)
+            ),
+        )
+        .subcommand(
+            //UNINSTALL
+            App::new("uninstall")
+            .about("卸载waddleup")
+            .arg(
+                Arg::new("stable")
+                .required(true)
+                .index(1)
+            ),
+        )
+        .subcommand(
+            //UPDATE
+            App::new("run")
+            .about("运行waddleup")
+            .arg(
+                Arg::new("stable")
+                .required(true)
+                .index(1)
+            ),
+        );
+
+    let matches = waddleup.get_matches();
+    let mut status = true;
+
+    //WADDLEUP INSTALL
+    if status {
+        matches.subcommand_matches("install").map(|_| {
+            status = false;
+            Command::new("powershell")
+            .args(["git", "clone", "https://gitee.com/coco-ag/coco-waddle.git"]);
+        });
+    }
+    if status {
+        matches.subcommand_matches("uninstall").map(|_| {
+            status = false;
+            Command::new("rm")
+            .args(["-rf", "coco-waddle"]);
+        });
+    }
+    if status{
+        matches.subcommand_matches("run").map(|_| {
+            status = false;
+            Command::new("powershell")
+            .args(["start http://localhost:8000/"]);
+            Command::new("powershell")
+            .args(["cd", "coco-waddle"])
+            .args(["python -m http.server 8000"]);
+        });
+    }
+    Ok(())
+}
