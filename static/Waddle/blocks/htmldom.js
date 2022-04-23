56e6246a79a4114e645fa2becd669b20f3681874
MathCalculus <code_math_calculus@163.com>
2022-04-23 12:49:18 +0800

    [feat]更新2块

--- static/Waddle/blocks/htmldom.js
+++ static/Waddle/blocks/htmldom.js
@@ -13,7 +13,31 @@ Blockly.Blocks['html_getelementbyid'] = {
         this.appendValueInput('NODE').setCheck('Node').appendField('从');
         this.appendValueInput('ID').setCheck(null).appendField('获取id为');
         this.appendDummyInput().appendField('的元素');
-        this.setOutput(true, null);
+        this.setOutput(true, 'Node');
+        this.setColour(230);
+        this.setTooltip('');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.Blocks['html_getelementbyclass'] = {
+    init: function () {
+        this.appendValueInput('NODE').setCheck('Node').appendField('从');
+        this.appendValueInput('CLASS').setCheck(null).appendField('获取class为');
+        this.appendDummyInput().appendField('的元素列表');
+        this.setOutput(true, 'Array');
+        this.setColour(230);
+        this.setTooltip('');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.Blocks['html_getelementbytagname'] = {
+    init: function () {
+        this.appendValueInput('NODE').setCheck('Node').appendField('从');
+        this.appendValueInput('TAGNAME').setCheck(null).appendField('获取标签名为');
+        this.appendDummyInput().appendField('的元素列表');
+        this.setOutput(true, 'Array');
         this.setColour(230);
         this.setTooltip('');
         this.setHelpUrl('');
--- static/Waddle/generators/javascript/htmldom.js
+++ static/Waddle/generators/javascript/htmldom.js
@@ -9,3 +9,17 @@ Blockly.JavaScript['html_getelementbyid'] = function (block) {
     var code = `${value_node}.getElementById(${value_id})`;
     return [code, Blockly.JavaScript.ORDER_NONE];
 };
+
+Blockly.JavaScript['html_getelementbyclass'] = function (block) {
+    var value_node = Blockly.JavaScript.valueToCode(block, 'NODE', Blockly.JavaScript.ORDER_ATOMIC) || 'document';
+    var value_id = Blockly.JavaScript.valueToCode(block, 'CLASS', Blockly.JavaScript.ORDER_ATOMIC) || "''";
+    var code = `${value_node}.getElementByClass(${value_id})`;
+    return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['html_getelementbytagname'] = function (block) {
+    var value_node = Blockly.JavaScript.valueToCode(block, 'NODE', Blockly.JavaScript.ORDER_ATOMIC) || 'document';
+    var value_id = Blockly.JavaScript.valueToCode(block, 'TAGNAME', Blockly.JavaScript.ORDER_ATOMIC) || "''";
+    var code = `${value_node}.getElementByTagName(${value_id})`;
+    return [code, Blockly.JavaScript.ORDER_NONE];
+};
