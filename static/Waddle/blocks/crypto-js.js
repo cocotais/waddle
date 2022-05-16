bd18d2060005e775d1ffe658168e074401f1cba9
MathCalculus <code_math_calculus@163.com>
2022-05-17 05:57:44 +0800

    [feat]更新2块

--- static/Waddle/blocks/crypto-js.js
+++ static/Waddle/blocks/crypto-js.js
@@ -13,7 +13,8 @@ Blockly.Blocks["crypto_md5"] = {
   init: function () {
     this.appendValueInput("STR")
       .setCheck(null)
-      .appendField("使用Crypto哈希MD5");
+      .appendField("使用Crypto生成哈希MD5");
+    this.appendDummyInput("").appendField("的对象");
     this.setOutput(true, null);
     this.setColour("#0682f5");
     this.setTooltip("");
@@ -21,4 +22,26 @@ Blockly.Blocks["crypto_md5"] = {
   },
 };
 
+Blockly.Blocks["crypto_sha1"] = {
+  init: function () {
+    this.appendValueInput("STR")
+      .setCheck(null)
+      .appendField("使用Crypto哈希SHA-1");
+    this.setOutput(true, null);
+    this.setColour("#0682f5");
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+
+Blockly.Blocks["crypto_tostr"] = {
+  init: function () {
+    this.appendValueInput("STR").setCheck(null);
+    this.appendDummyInput().appendField("到字符串");
+    this.setOutput(true, "String");
+    this.setColour("#0892f8");
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
 //用甚莫颜色好？
--- static/Waddle/generators/javascript/crypto-js.js
+++ static/Waddle/generators/javascript/crypto-js.js
@@ -11,3 +11,25 @@ Blockly.JavaScript["crypto_md5"] = function (block) {
   var code = `CryptoJS.MD5(${value_str})`;
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
+
+Blockly.JavaScript["crypto_sha1"] = function (block) {
+  var value_str = Blockly.JavaScript.valueToCode(
+    block,
+    "STR",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  var code = `CryptoJS.SHA1(${value_str})`;
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript["crypto_tostr"] = function (block) {
+  var value_str = Blockly.JavaScript.valueToCode(
+    block,
+    "STR",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  // TODO: Assemble JavaScript into code variable.
+  var code = `(${value_str}).toString()`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
