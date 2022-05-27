2613a66196ba3bbcf60b7a3fa7debd7397fc7c9a
sf-yuzifu <yuzifu@yuzifu.top>
2022-05-28 00:25:06 +0800

    [fix]修复积木栏重复显示问题、[perf]优化文件大小（删除几个占用过大彩蛋）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Waddle-'+'V1.46.22(965)-beta'//也要改我！;
+const CACHE_NAME = 'Waddle-'+'V1.46.23(969)-beta'//也要改我！;
 const FILES_TO_CACHE = ['./',
   './index.html',
   './static/Waddle/toolBox.xml',
