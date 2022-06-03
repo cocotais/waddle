b11037d0d3730f3af16ab594d6eb7fd1b667c60f
MathCalculus <code_math_calculus@163.com>
2022-06-04 07:08:07 +0800

    [feat]更新彩蛋游戏

--- static/Waddle/blocks/what.js
+++ static/Waddle/blocks/what.js
@@ -26,19 +26,35 @@ Blockly.Blocks["what_waddle1"] = {
   },
 };
 //嘿嘿，我只做了一个
-Blockly.Blocks["game_tictactoe_board1"] = function () {
+Blockly.JavaScript["game_tictactoe_board1"] = function () {
   return "";
 };
-Blockly.Blocks["game_tictactoe_board2"] = function () {
+Blockly.JavaScript["game_tictactoe_board2"] = function () {
   return "";
 };
-Blockly.Blocks["game_tictactoe_board3"] = function () {
+Blockly.JavaScript["game_tictactoe_board3"] = function () {
   return "";
 };
-Blockly.Blocks["game_tictactoe_o"] = function () {
+Blockly.JavaScript["game_tictactoe_o"] = function () {
   return "";
 };
-Blockly.Blocks["game_tictactoe_x"] = function () {
+Blockly.JavaScript["game_tictactoe_x"] = function () {
+  return "";
+};
+Blockly.Blocks["game_idiom_paper"] = {
+  init: function () {
+    this.appendDummyInput()
+      .appendField("猜成语")
+      .appendField(new Blockly.FieldTextInput("字"), "1")
+      .appendField(new Blockly.FieldTextInput("字"), "2")
+      .appendField(new Blockly.FieldTextInput("字"), "3")
+      .appendField(new Blockly.FieldTextInput("字"), "4");
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.JavaScript["game_idiom_paper"] = function () {
   return "";
 };
 /*function load_game() {
@@ -74,3 +90,217 @@ Blockly.Blocks["game_tictactoe_x"] = function () {
     document.getElementById("toolbox").innerHTML=document.getElementById("toolbox").innerHTML+str
 }
 */
+
+Blockly.Blocks["game_chinese_chess_chessboard1"] = {
+  init: function () {
+    this.appendDummyInput().appendField("======象棋======");
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.Blocks["game_chinese_chess_chessboard2"] = {
+  init: function () {
+    this.appendValueInput("1").setCheck(null);
+    this.appendValueInput("2").setCheck(null);
+    this.appendValueInput("3").setCheck(null);
+    this.appendValueInput("4").setCheck(null);
+    this.appendValueInput("5").setCheck(null);
+    this.appendValueInput("6").setCheck(null);
+    this.appendValueInput("7").setCheck(null);
+    this.appendValueInput("8").setCheck(null);
+    this.appendValueInput("9").setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.Blocks["game_chinese_chess_chessboard3"] = {
+  init: function () {
+    this.appendValueInput("1").setCheck(null);
+    this.appendValueInput("2").setCheck(null);
+    this.appendValueInput("3").setCheck(null);
+    this.appendValueInput("4").setCheck(null);
+    this.appendValueInput("5").setCheck(null);
+    this.appendValueInput("6").setCheck(null);
+    this.appendValueInput("7").setCheck(null);
+    this.appendValueInput("8").setCheck(null);
+    this.appendValueInput("9").setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.Blocks["game_chinese_chess_black"] = {
+  init: function () {
+    this.appendDummyInput().appendField(new Blockly.FieldTextInput(""), "NAME");
+    this.setOutput(true, null);
+    this.setColour("#25332c");
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.Blocks["game_chinese_chess_red"] = {
+  init: function () {
+    this.appendDummyInput().appendField(new Blockly.FieldTextInput(""), "NAME");
+    this.setOutput(true, null);
+    this.setColour(0);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.Blocks["game_chinese_chess_chessboard4"] = {
+  init: function () {
+    this.appendDummyInput().appendField("=========楚河  漢界=========");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.JavaScript["game_chinese_chess_chessboard1"] = function () {
+  return "";
+};
+Blockly.JavaScript["game_chinese_chess_chessboard2"] = function () {
+  return "";
+};
+Blockly.JavaScript["game_chinese_chess_chessboard3"] = function () {
+  return "";
+};
+Blockly.JavaScript["game_chinese_chess_chessboard4"] = function () {
+  return "";
+};
+
+Blockly.JavaScript["game_chinese_chess_black"] = function () {
+  return "";
+};
+
+Blockly.JavaScript["game_chinese_chess_red"] = function () {
+  return "";
+};
+Blockly.Blocks["game_weiqi_chessborad3"] = {
+  init: function () {
+    this.appendValueInput("1").setCheck(null);
+    this.appendValueInput("2").setCheck(null);
+    this.appendValueInput("3").setCheck(null);
+    this.appendValueInput("4").setCheck(null);
+    this.appendValueInput("5").setCheck(null);
+    this.appendValueInput("6").setCheck(null);
+    this.appendValueInput("7").setCheck(null);
+    this.appendValueInput("8").setCheck(null);
+    this.appendValueInput("9").setCheck(null);
+    this.appendValueInput("10").setCheck(null);
+    this.appendValueInput("11").setCheck(null);
+    this.appendValueInput("12").setCheck(null);
+    this.appendValueInput("13").setCheck(null);
+    this.appendValueInput("14").setCheck(null);
+    this.appendValueInput("15").setCheck(null);
+    this.appendValueInput("16").setCheck(null);
+    this.appendValueInput("17").setCheck(null);
+    this.appendValueInput("18").setCheck(null);
+    this.appendValueInput("19").setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.Blocks["game_weiqi_chessborad2"] = {
+  init: function () {
+    this.appendValueInput("1").setCheck(null);
+    this.appendValueInput("2").setCheck(null);
+    this.appendValueInput("3").setCheck(null);
+    this.appendValueInput("4").setCheck(null);
+    this.appendValueInput("5").setCheck(null);
+    this.appendValueInput("6").setCheck(null);
+    this.appendValueInput("7").setCheck(null);
+    this.appendValueInput("8").setCheck(null);
+    this.appendValueInput("9").setCheck(null);
+    this.appendValueInput("10").setCheck(null);
+    this.appendValueInput("11").setCheck(null);
+    this.appendValueInput("12").setCheck(null);
+    this.appendValueInput("13").setCheck(null);
+    this.appendValueInput("14").setCheck(null);
+    this.appendValueInput("15").setCheck(null);
+    this.appendValueInput("16").setCheck(null);
+    this.appendValueInput("17").setCheck(null);
+    this.appendValueInput("18").setCheck(null);
+    this.appendValueInput("19").setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.Blocks["game_weiqi_chessborad1"] = {
+  init: function () {
+    this.appendDummyInput().appendField("===============围棋===============");
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.Blocks["game_weiqi_black"] = {
+  init: function () {
+    this.setOutput(true, null);
+    this.setColour("#000");
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.Blocks["game_weiqi_white"] = {
+  init: function () {
+    this.setOutput(true, null);
+    this.setColour("#fff");
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+
+Blockly.JavaScript["game_weiqi_black"] = function () {
+  return "";
+};
+
+Blockly.JavaScript["game_weiqi_white"] = function () {
+  return "";
+};
+
+Blockly.JavaScript["game_weiqi_chessborad1"] = function () {
+  return "";
+};
+
+Blockly.JavaScript["game_weiqi_chessborad2"] = function () {
+  return "";
+};
+Blockly.JavaScript["game_weiqi_chessborad1"] = function () {
+  return "";
+};
+Blockly.JavaScript["game_weiqi_chessborad3"] = function () {
+  return "";
+};
+Blockly.Blocks["game_count"] = {
+  init: function () {
+    this.appendDummyInput().appendField(" = 计数器 =");
+    this.appendDummyInput()
+      .appendField("黑")
+      .appendField(new Blockly.FieldTextInput("0"), "b")
+      .appendField("白")
+      .appendField(new Blockly.FieldTextInput("0"), "w");
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+Blockly.JavaScript["game_count"]=function(){return ''}
