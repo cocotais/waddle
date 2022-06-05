0d8e66ce33ef5247ae66c92a79ce251052dea2ec
technology-me <lishangmingxuan@foxmail.com>
2022-06-05 08:53:22 +0000

    [version]端午节新版本

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Waddle-'+'V1.47.27(1015)-beta'//也要改我！;
+const CACHE_NAME = 'Waddle-'+'V1.47.27(1015)-release'//也要改我！;
 const FILES_TO_CACHE = ['./',
   './index.html',
   './static/Waddle/toolBox.xml',
