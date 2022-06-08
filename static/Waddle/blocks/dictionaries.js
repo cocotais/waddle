f40ca6ee1cbb6225019f69fdbf04243fe5822113
technology-me <lishangmingxuan@foxmail.com>
2022-06-09 00:05:27 +0800

    [feat]更新若干积木

--- static/Waddle/blocks/dictionaries.js
+++ static/Waddle/blocks/dictionaries.js
@@ -7,9 +7,139 @@ Blockly.Blocks['dict_pair'] = {
             .setCheck(null)
             .appendField("值");
         this.setInputsInline(true);
-        this.setOutput(true, null);
+        this.setOutput(true,'dict_pair');
         this.setColour(BlockColors["dict"]);
         this.setTooltip("生成一个键值对");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['dict_set'] = {
+    init: function () {
+        this.appendValueInput("key")
+            .setCheck(null)
+            .appendField("键");
+        this.appendValueInput("value")
+            .setCheck(null)
+            .appendField("值");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(BlockColors["dict"]);
+        this.setTooltip("设置一个键值对");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['dict_get'] = {  
+    init: function () {
+        this.appendDummyInput()
+            .appendField("获取键")
+        this.appendValueInput("key")
+            .setCheck(null)
+            .appendField("键");
+        this.appendDummyInput()
+            .appendField("的值");
+        this.setInputsInline(true);
+        this.setOutput(true, null);
+        this.setColour(BlockColors["dict"]);
+        this.setTooltip("获取一个键值对");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['dict_del'] = {
+    init: function () {
+        this.appendValueInput("dict")
+            .setCheck(null)
+            .appendField("删除字典");
+        this.appendValueInput("key")
+            .setCheck(null)
+            .appendField("键")
+        this.appendDummyInput()
+            .appendField("的值");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(BlockColors["dict"]);
+        this.setTooltip("删除一个键值对");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['dict_clear'] = {
+    init: function () {
+        this.appendValueInput("dict")
+            .setCheck(null)
+            .appendField("清空字典");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(BlockColors["dict"]);
+        this.setTooltip("清空字典");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['dict_len'] = {
+    init: function () {
+        this.appendValueInput("dict")
+            .setCheck(null)
+            .appendField("字典");
+        this.appendDummyInput()
+            .appendField("的长度");
+        this.setInputsInline(true);
+        this.setOutput(true, null);
+        this.setColour(BlockColors["dict"]);
+        this.setTooltip("获取字典的长度");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['dict_keys'] = { 
+    init: function () {
+        this.appendValueInput("dict")
+            .setCheck(null)
+            .appendField("字典");
+        this.appendDummyInput()
+            .appendField("的所有键");
+        this.setInputsInline(true);
+        this.setOutput(true, null);
+        this.setColour(BlockColors["dict"]);
+        this.setTooltip("获取字典的键");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['dict_values'] = {
+    init: function () {
+        this.appendValueInput("dict")
+            .setCheck(null)
+            .appendField("字典");
+        this.appendDummyInput()
+            .appendField("的所有值");
+        this.setInputsInline(true);
+        this.setOutput(true, null);
+        this.setColour(BlockColors["dict"]);
+        this.setTooltip("获取字典的值");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['dict_has_key'] = {
+    init: function () {
+        this.appendValueInput("dict")
+            .setCheck(null)
+            .appendField("字典");
+        this.appendValueInput("key")
+            .setCheck(null)
+            .appendField("键");
+        this.appendDummyInput()
+            .appendField("是否存在");
+        this.setInputsInline(true);
+        this.setOutput(true, 'Boolean');
+        this.setColour(BlockColors["dict"]);
+        this.setTooltip("判断字典是否存在某个键");
         this.setHelpUrl("");
     }
 };
--- static/Waddle/generators/javascript/dictionaries.js
+++ static/Waddle/generators/javascript/dictionaries.js
@@ -3,3 +3,46 @@ Blockly.JavaScript['dict_pair'] = function (block) {
     var value = Blockly.JavaScript.valueToCode(block, 'value', Blockly.JavaScript.ORDER_ATOMIC);
     return '{' + key + ':' + value + '}';
 }
+
+Blockly.JavaScript['dict_set'] = function (block) {
+    var key = Blockly.JavaScript.valueToCode(block, 'key', Blockly.JavaScript.ORDER_ATOMIC);
+    var value = Blockly.JavaScript.valueToCode(block, 'value', Blockly.JavaScript.ORDER_ATOMIC);
+    return 'dict.set(' + key + ',' + value + ');\n';
+}
+
+Blockly.JavaScript['dict_get'] = function (block) {
+    var key = Blockly.JavaScript.valueToCode(block, 'key', Blockly.JavaScript.ORDER_ATOMIC);
+    return 'dict.get(' + key + ')';
+}
+
+Blockly.JavaScript['dict_del'] = function (block) {
+    var dict = Blockly.JavaScript.valueToCode(block, 'dict', Blockly.JavaScript.ORDER_ATOMIC);
+    var key = Blockly.JavaScript.valueToCode(block, 'key', Blockly.JavaScript.ORDER_ATOMIC);
+    return dict + '.del(' + key + ');\n';
+}
+
+Blockly.JavaScript['dict_clear'] = function (block) {
+    var dict = Blockly.JavaScript.valueToCode(block, 'dict', Blockly.JavaScript.ORDER_ATOMIC);
+    return dict + '.clear();\n';
+}
+
+Blockly.JavaScript['dict_len'] = function (block) {
+    var dict = Blockly.JavaScript.valueToCode(block, 'dict', Blockly.JavaScript.ORDER_ATOMIC);
+    return 'Object.keys(' + dict + ').length';
+}
+
+Blockly.JavaScript['dict_keys'] = function (block) {
+    var dict = Blockly.JavaScript.valueToCode(block, 'dict', Blockly.JavaScript.ORDER_ATOMIC);
+    return 'Object.keys(' + dict + ')';
+}
+
+Blockly.JavaScript['dict_values'] = function (block) {
+    var dict = Blockly.JavaScript.valueToCode(block, 'dict', Blockly.JavaScript.ORDER_ATOMIC);
+    return 'Object.values(' + dict + ')';
+}
+
+Blockly.JavaScript['dict_has_key'] = function (block) {
+    var dict = Blockly.JavaScript.valueToCode(block, 'dict', Blockly.JavaScript.ORDER_ATOMIC);
+    var key = Blockly.JavaScript.valueToCode(block, 'key', Blockly.JavaScript.ORDER_ATOMIC);
+    return '(' + key + ' in ' + dict + ')';
+}
