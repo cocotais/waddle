2276a4314e2b1b00fb2a4444ae907ea11e2ad387
sf-yuzifu <yuzifu@yuzifu.top>
2022-06-24 06:05:24 +0800

    [fix]小修小改

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Waddle-'+'V1.47.35(1034)-beta'//也要改我！;
+const CACHE_NAME = 'Waddle-'+'V1.47.36(1035)-beta'//也要改我！;
 const FILES_TO_CACHE = ['./',
   './index.html',
   './static/Waddle/toolBox.xml',
