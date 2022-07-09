16e549789e41a50e173558b99af210e8e291d0f7
中子星000 <f12y34m56@163.com>
2022-07-09 10:21:36 +0800

    Revert "[feat]新增弹窗显示"
    This reverts commit 073c4d4d2139e4c2ef4c4b3b38a7501252949703.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Waddle-'+'V1.47.53(1096)-beta'//也要改我！;
+const CACHE_NAME = 'Waddle-'+'V1.47.52(1093)-beta'//也要改我！;
 const FILES_TO_CACHE = ['./',
   './index.html',
   './static/Waddle/toolBox.xml',
