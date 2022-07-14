406b6a70ad93cca91a2e733124baa0d41fff6296
小鱼yuzifu <yuzifu@yuzifu.top>
2022-07-14 09:34:34 +0800

    [version]版本同步（tmd谁搞的）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Waddle-'+'V1.47.55(1138)-beta'//也要改我！;
+const CACHE_NAME = 'Waddle-'+'V1.47.56(1143)-beta'//也要改我！;
 const FILES_TO_CACHE = ['./',
   './index.html',
   './static/Waddle/toolBox.xml',
