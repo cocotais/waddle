a334083c2b06bb28f3d8d8228f6f5239dc2d9aa3
中子星000 <f12y34m56@163.com>
2022-07-14 11:25:33 +0800

    [fix]修复了无法导出的bug（也就是加个提示）

--- static/js/navFuncs.js
+++ static/js/navFuncs.js
@@ -6,28 +6,34 @@
 }`;
 
 let js_save = function () {
-  let code_data = Blockly.JavaScript.workspaceToCode(workspace)
-    .replaceAll("&#60", "<")
-    .replaceAll("&#62", ">");
-  let blob = new Blob([code_data], { type: "text/plain;charset=utf-8" });
-  let url = URL.createObjectURL(blob);
-  let downa = document.getElementById("downa");
-  downa.href = url;
-  let name = mytitle;
-  if (name == "") {
-    name = "我的控件";
+  try {
+    let code_data = Blockly.JavaScript.workspaceToCode(workspace);
+    code_data = code_data
+      .replaceAll("&#60", "<")
+      .replaceAll("&#62", ">");
+    let blob = new Blob([code_data], { type: "text/plain;charset=utf-8" });
+    let url = URL.createObjectURL(blob);
+    let downa = document.getElementById("downa");
+    downa.href = url;
+    let name = mytitle;
+    if (name == "") {
+      name = "我的控件";
+    }
+    if (
+      code_data.indexOf(
+        "isInvisibleWidget: true"
+      ) == -1
+    ) {
+      downa.download = name + ".jsx";
+    } else {
+      downa.download = name + ".js";
+    }
+    downa.click();
+    URL.revokeObjectURL(url);
+  } catch (error) {
+    swal("导出出现问题，请检查积木是否拼接错误，如无误请反馈给Waddle开发人员")
   }
-  if (
-    Blockly.JavaScript.workspaceToCode(workspace).indexOf(
-      "isInvisibleWidget: true"
-    ) == -1
-  ) {
-    downa.download = name + ".jsx";
-  } else {
-    downa.download = name + ".js";
-  }
-  downa.click();
-  URL.revokeObjectURL(url);
+
 };
 
 let format_data = function (text, js) {
@@ -35,21 +41,25 @@ let format_data = function (text, js) {
 };
 
 let save = function () {
-  let code_xml = Blockly.Xml.workspaceToDom(workspace);
-  let code_data = Blockly.Xml.domToText(code_xml);
-  let js_data = Blockly.JavaScript.workspaceToCode(workspace);
-  let xml_data = format_data(code_data, js_data);
-  let blob = new Blob([xml_data], { type: "text/plain;charset=utf-8" });
-  let url = URL.createObjectURL(blob);
-  let downa = document.getElementById("downa");
-  downa.href = url;
-  let name = mytitle;
-  if (name == "") {
-    name = "我的控件";
+  try {
+    let code_xml = Blockly.Xml.workspaceToDom(workspace);
+    let code_data = Blockly.Xml.domToText(code_xml);
+    let js_data = Blockly.JavaScript.workspaceToCode(workspace);
+    let xml_data = format_data(code_data, js_data);
+    let blob = new Blob([xml_data], { type: "text/plain;charset=utf-8" });
+    let url = URL.createObjectURL(blob);
+    let downa = document.getElementById("downa");
+    downa.href = url;
+    let name = mytitle;
+    if (name == "") {
+      name = "我的控件";
+    }
+    downa.download = name + ".waddle";
+    downa.click();
+    URL.revokeObjectURL(url);
+  } catch (error) {
+    swal("导出出现问题，请检查积木是否拼接错误，如无误请反馈给Waddle开发人员")
   }
-  downa.download = name + ".waddle";
-  downa.click();
-  URL.revokeObjectURL(url);
 };
 
 let openfile = function () {
@@ -181,22 +191,22 @@ function show_newsth() {
 
 
 function settings(check) {
-  if(check.checked){
+  if (check.checked) {
     window.settingss = '';
   }
-  else{
+  else {
     window.settingss = 'keep';
   }
-  window.reast = 'minus';document.getElementsByClassName('blocklyZoom')[0].dispatchEvent(new PointerEvent('pointerdown'));window.reast = false;document.getElementById('zoom-size-number').innerHTML = Math.ceil(myscale * 100)+'%'
-  window.reast = 'plus';document.getElementsByClassName('blocklyZoom')[1].dispatchEvent(new PointerEvent('pointerdown'));window.reast = false;document.getElementById('zoom-size-number').innerHTML = Math.ceil(myscale * 100)+'%'
+  window.reast = 'minus'; document.getElementsByClassName('blocklyZoom')[0].dispatchEvent(new PointerEvent('pointerdown')); window.reast = false; document.getElementById('zoom-size-number').innerHTML = Math.ceil(myscale * 100) + '%'
+  window.reast = 'plus'; document.getElementsByClassName('blocklyZoom')[1].dispatchEvent(new PointerEvent('pointerdown')); window.reast = false; document.getElementById('zoom-size-number').innerHTML = Math.ceil(myscale * 100) + '%'
   document.cookie = "settingss=" + window.settingss;
 }
 
 function holdToolboxs(check) {
-  if(check.checked){
+  if (check.checked) {
     window.holdToolbox = 'true';
   }
-  else{
+  else {
     window.holdToolbox = '';
   }
   document.cookie = "holdToolbox=" + window.holdToolbox;
@@ -207,10 +217,10 @@ function settingThird(number) {
   b = document.getElementsByClassName("aboutus-body")[0]
   c = document.getElementsByClassName("thanks-body")[0]
   d = document.getElementsByClassName("setting-button")
-  for (i = 0; i < d.length; i++) { 
+  for (i = 0; i < d.length; i++) {
     d[i].style.backgroundColor = 'var(--settings-button-background)'
     d[i].style.color = 'var(--settings-button-color)'
- }
+  }
   if (number == 1) {
     a.style.display = 'flex'
     b.style.display = 'none'
