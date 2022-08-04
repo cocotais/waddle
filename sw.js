f3813844c8a37fb694c6f5fe963d56be5cf2e0bc
小鱼yuzifu <yuzifu@yuzifu.top>
2022-08-04 21:25:04 +0800

    [fix]修复图标显示问题

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var version = "v1.50.13(1223)-beta"; // 设置版本号
+var version = "v1.50.14(1226)-beta"; // 设置版本号
 
 self.addEventListener("install", function (event) { // serviceworker第一次加载的时候调用，可以在此时缓存静态资源
   event.waitUntil(
