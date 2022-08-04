5b001de2403e19b7cf940d42c1c25f5d5e4ee144
小鱼yuzifu <yuzifu@yuzifu.top>
2022-08-04 11:44:27 +0800

    [fix]sw尝试修复

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var version = "v1.50.11(1221)-beta"; // 设置版本号
+var version = "v1.50.12(1222)-beta"; // 设置版本号
 
 self.addEventListener("install", function (event) { // serviceworker第一次加载的时候调用，可以在此时缓存静态资源
   event.waitUntil(
