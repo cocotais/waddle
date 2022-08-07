3bfa66daab60d6660b0c87c4bed233010cff0482
liulyxandy <liuyuxuanandy@outlook.com>
2022-08-07 10:10:24 +0000

    [fix]修复积木名称

--- static/Waddle/blocks/pedometer.js
+++ static/Waddle/blocks/pedometer.js
@@ -1,6 +1,6 @@
 Blockly.Blocks["pedometer_import"] = {
   init: function () {
-    this.appendDummyInput().appendField("引入http");
+    this.appendDummyInput().appendField("引入pedometer");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour("#3d92ef");
