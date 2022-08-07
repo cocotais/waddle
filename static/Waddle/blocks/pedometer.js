e7c8aeb48dfef0fa705cf7eaa8bca22ae9400280
中子星000 <f12y34m56@163.com>
2022-08-07 13:29:14 +0800

    [pref]新增两库（还没有积木）

---
+++ static/Waddle/blocks/pedometer.js
@@ -0,0 +1,10 @@
+Blockly.Blocks["pedometer_import"] = {
+  init: function () {
+    this.appendDummyInput().appendField("引入http");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour("#3d92ef");
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
