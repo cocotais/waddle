38ec5a46e6079cee02f2ea7ff82a3d733e4abbdd
王晨帆 <12681695+wang-chenfan@user.noreply.gitee.com>
2023-07-19 05:23:46 +0000

    未完成
    Signed-off-by: 王晨帆 <12681695+wang-chenfan@user.noreply.gitee.com>

--- static/Waddle/blocks/http.js
+++ static/Waddle/blocks/http.js
@@ -8,3 +8,16 @@ Blockly.Blocks["http_import"] = {
     this.setHelpUrl("");
   },
 };
+
+Blockly.Blocks["http_get"] = {
+  init: function () {
+    this.appendValueInput("URL")
+      .setCheck(null)
+      .appendField("使用http 发送GET请求 链接");
+    this.setInputsInline(true);
+    this.setOutput(false,null);
+    this.setColour("#3d92ef");
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
--- static/Waddle/generators/javascript/http.js
+++ static/Waddle/generators/javascript/http.js
@@ -2,3 +2,13 @@ Blockly.JavaScript["http_import"] = function (block) {
   var code = "const http = require('http');\n";
   return code;
 };
+
+Blockly.JavaScript["http_get"] = function (block) {
+  var value_url = Blockly.JavaScript.valueToCode(
+    block,
+    "URL",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  var code = `http.get(${value_url})`;
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
