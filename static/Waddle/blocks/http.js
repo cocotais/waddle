2fe56bfe59c097e03c3edc8e663144bcb9126219
王晨帆 <12681695+wang-chenfan@user.noreply.gitee.com>
2023-07-19 11:45:25 +0000

    哈哈，终于完成了！
    Signed-off-by: 王晨帆 <12681695+wang-chenfan@user.noreply.gitee.com>

--- static/Waddle/blocks/http.js
+++ static/Waddle/blocks/http.js
@@ -12,10 +12,63 @@ Blockly.Blocks["http_import"] = {
 Blockly.Blocks["http_get"] = {
   init: function () {
     this.appendValueInput("URL")
+      .setCheck("String")
+      .appendField("使用http")
+      .appendField(
+        new Blockly.FieldDropdown([
+          ["get", "get"],
+          ["post", "post"],
+        ]),
+        "MODE"
+      )
+      .appendField("链接");
+    this.appendStatementInput("OK").setCheck(null).appendField("当请求完成时");
+    this.appendStatementInput("ERROR")
       .setCheck(null)
-      .appendField("使用http 发送GET请求 链接");
+      .appendField("当请求失败时");
     this.setInputsInline(true);
-    this.setOutput(false,null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour("#3d92ef");
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+
+Blockly.Blocks["http_response"] = {
+  init: function () {
+    this.appendDummyInput().appendField("响应内容");
+    this.setOutput(true, null);
+    this.setColour("#3d92ef");
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+
+Blockly.Blocks["http_responsedropdown"] = {
+  init: function () {
+    this.appendDummyInput()
+      .appendField("响应内容的")
+      .appendField(
+        new Blockly.FieldDropdown([
+          ["信息", "data"],
+          ["响应头", "headers"],
+          ["HTTP状态码", "status"],
+          ["HTTP状态信息", "statusText"],
+        ]),
+        "MODE"
+      );
+    this.setOutput(true, null);
+    this.setColour("#3d92ef");
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+};
+
+Blockly.Blocks["http_error"] = {
+  init: function () {
+    this.appendDummyInput().appendField("获取错误");
+    this.setOutput(true, null);
     this.setColour("#3d92ef");
     this.setTooltip("");
     this.setHelpUrl("");
--- static/Waddle/generators/javascript/http.js
+++ static/Waddle/generators/javascript/http.js
@@ -4,11 +4,35 @@ Blockly.JavaScript["http_import"] = function (block) {
 };
 
 Blockly.JavaScript["http_get"] = function (block) {
-  var value_url = Blockly.JavaScript.valueToCode(
-    block,
-    "URL",
-    Blockly.JavaScript.ORDER_ATOMIC
-  );
-  var code = `http.get(${value_url})`;
-  return [code, Blockly.JavaScript.ORDER_NONE];
+    var mode = block.getFieldValue('MODE');
+    var ok = Blockly.JavaScript.statementToCode(block, 'OK');
+    var error = Blockly.JavaScript.statementToCode(block, 'ERROR');
+    var url = Blockly.JavaScript.valueToCode(block, 'URL', Blockly.JavaScript.ORDER_ATOMIC) || "''";
+    var code = `https.${mode}(${url},{},{},
+   (response) => {
+        if (response.data) {
+          try {
+            ${ok}
+          } catch (e) {
+            ${error}
+          }
+        }
+      }
+    );
+`;
+    return code;
+};
+
+Blockly.JavaScript['axios_response'] = function () {
+    return ['response', Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['axios_responsedropdown'] = function (block) {
+    var dropdown_mode = block.getFieldValue('MODE');
+        var code = `response.${dropdown_mode}`;
+    return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['axios_error'] = function () {
+    return ['e', Blockly.JavaScript.ORDER_NONE];
 };
