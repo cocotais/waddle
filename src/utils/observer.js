20ca8b7e06c692c3c7cf5399f936b30a324839bc
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-06-17 18:33:38 +0800

    [fix] observer函数覆写问题

--- src/utils/observer.js
+++ src/utils/observer.js
@@ -5,14 +5,14 @@
 function observer(selector, attributeFilter, callback) {
   let element = document.querySelector(selector);
   // eslint-disable-next-line no-func-assign
-  observer = new window.MutationObserver(function (mutations) {
+  let elementObserver = new window.MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
       if (mutation.type === "attributes") {
         callback(element);
       }
     });
   });
-  observer.observe(element, {
+  elementObserver.observe(element, {
     attributes: true,
     attributeFilter: attributeFilter,
   });
